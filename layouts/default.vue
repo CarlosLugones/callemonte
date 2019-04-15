95b46142b9d26092556f8bb1308bb8f2f41e31f5
Jefe Grupo Redes Informáticas (Damian Soto Platero) <jefe.redes@gaviota.cu>
2019-04-15 12:03:54 -0400

    update

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,5 @@
 <template>
   <div>
     <nuxt />
-    <footer class="footer">
-      <div class="content has-text-centered">
-        <p>
-          <a href="https://medium.com/unclic">Blog</a>
-        </p>
-      </div>
-    </footer>  
   </div>
 </template>
