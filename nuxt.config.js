874642b28f760c8d45013867c71702add5c1a375
Jefe Grupo Redes Informáticas (Damian Soto Platero) <jefe.redes@gaviota.cu>
2019-04-16 09:03:03 -0400

    add favicon and meta social media

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,6 +16,10 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'apple-touch-icon', sizes: "180x180",  href: '/apple-touch-icon.ico' }
+      { rel: 'icon', type: 'image/png', sizes: "32x32", href: '/favicon-32x32.png' }
+      { rel: 'icon', type: 'image/png', sizes: "16x16", href: '/favicon-16x16.png' }
+      { rel: 'manifest',  href: '/site.webmanifest' }
     ]
   },
 
