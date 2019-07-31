40660c69d1038b7ae785b638fc52b4d17253e29d
Jefe Grupo Redes Informáticas (Damian Soto Platero) <soto.platero@gmail.com>
2019-07-31 10:16:05 -0400

    update

--- fn/phone.js
+++ fn/phone.js
@@ -40,7 +40,6 @@ exports.handler =  async (event, context, callback) => {
         .catch( err => console.error(err) );
 
     return {
-        headers,
         statusCode: 200,
         body: JSON.stringify( phones.join(', ') )
     };
