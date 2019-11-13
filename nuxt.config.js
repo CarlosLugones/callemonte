c3eb0c04bbc487fc37d7efcb80135fe9d71cf905
Jefe Grupo Redes Informáticas (Damian Soto Platero) <soto.platero@gmail.com>
2019-11-13 07:56:38 -0500

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -51,7 +51,7 @@ export default {
   */
   plugins: [
     // '~/plugins/axios',
-    { src: '~/plugins/vue-gallery.js', ssr: false }
+    { src: '~/plugins/vue-gallery.js', mode: 'client' }
     // { src: '~/plugins/vue-preload.js', ssr: false }
   ],
 
