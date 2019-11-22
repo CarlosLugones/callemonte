61068d6ac0b1b392e0f44d1699932ac4fa1ffc6e
Jefe Grupo Redes Informáticas (Damian Soto Platero) <soto.platero@gmail.com>
2019-11-22 08:30:44 -0500

    update

--- pages/search.vue
+++ pages/search.vue
@@ -63,17 +63,17 @@
                 <div class="mb-3"><b>Si te gustó Callemonte compártelo y corre la bola.</b></div>
                 <social-sharing url="https://callemonte.com/" title="Callemonte. Buscador de clasificados" description="Busca clasificados en los sitios de Cuba y ahorra tus datos" inline-template>  
                   <div class="d-flex justify-content-center">
-                    <network network="twitter" class="mr-3" style="cursor: pointer;">
-                        <img height="32" width="32" src="https://unpkg.com/simple-icons@latest/icons/twitter.svg" /> Twitter
+                    <network network="twitter" class="mr-4" style="cursor: pointer;">
+                        <img width="32" src="https://unpkg.com/simple-icons@latest/icons/twitter.svg" title="Twitter" att="Twitter"/> 
                     </network> 
-                    <network network="facebook" class="mr-3" style="cursor: pointer;">
-                      <img height="32" width="32" src="https://unpkg.com/simple-icons@latest/icons/facebook.svg" /> Facebook
+                    <network network="facebook" class="mr-4" style="cursor: pointer;">
+                      <img width="32" src="https://unpkg.com/simple-icons@latest/icons/facebook.svg" title="Facebook" att="Twitter"/> 
                     </network>                        
-                    <network network="telegram" class="mr-3" style="cursor: pointer;">
-                      <img height="32" width="32" src="https://unpkg.com/simple-icons@latest/icons/telegram.svg" /> Telegram
+                    <network network="telegram" class="mr-4" style="cursor: pointer;">
+                      <img width="32" src="https://unpkg.com/simple-icons@latest/icons/telegram.svg" title="Telegram" att="Twitter"/> 
                     </network>     
                     <network network="whatsapp" style="cursor: pointer;">
-                      <img height="32" width="32" src="https://unpkg.com/simple-icons@latest/icons/whatsapp.svg" /> Whatsapp
+                      <img width="32" src="https://unpkg.com/simple-icons@latest/icons/whatsapp.svg" title="Whatsapp" att="Twitter"/> 
                     </network>     
                   </div>    
                   </social-sharing>  
