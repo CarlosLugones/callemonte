fab42c79446cac96f225dcce28945aabc7146890
Damian Soto Platero <jefe.redes@gaviota.cu>
2020-01-20 08:39:16 -0500

    incluyendo link al repo

--- fn/bachecubano.js
+++ fn/bachecubano.js
@@ -6,20 +6,19 @@ const rePhone = /(\+?53)?\s?([1-9][\s-]?){1}(\d[\s-]?){7}/g;
 
 exports.handler =  async (event, context, callback) => {
     const { q, p = 1 } = event.queryStringParameters;
-
-    const response = await fetch(`https://www.bachecubano.com/search/pattern,${q}/iPage,${p}/sPriceMin,1`);
+    const response = await fetch(`https://www.bachecubano.com/search?s=${q}&min_price=${p}`);
     const body = await response.text();
     const $ = cheerio.load( body );
 
-    let data = $('.simple-wrap').map( (i,el) => {
+    let data = $('.product-item').map( (i,el) => {
         let $el = $(el), 
-            $a = $el.find('a.title');
+            $a = $el.find('.product-title a');
 
         return {
             id:     "B" + $a.attr('href').match(/\d+$/)[0],
             price:  parseInt(($el.find('.price span').text() || 0).replace(/[^\d\.,]/g,'')),
             photo:  !$el.find('a.img-link').hasClass('no-img'),
-            title:  cleaner( $a.children().remove().end().text() ),
+            title:  cleaner( $a.text() ),
             phones: $a.text().replace(/\W/g,'').match(rePhone) || [],
             url:    $a.attr('href'),
             date: ''
